35417f7303a8190f6ae58de109c21c4d0d56928f
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2022-09-01 16:51:37 -0400

    Allowing user to make request to both local and global servers

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,7 +3,7 @@ export const getApiUrl = () => {
 }
 
 export const getWSApiUrl = () => {
-    return "ws://10.0.0.169:5000/graphql";
+    return  `ws://${process.env.DOMAIN || "10.0.0.169:5000"}/graphql`;
 }
 
 export const getImageUploadApi = () => {
@@ -11,5 +11,5 @@ export const getImageUploadApi = () => {
 }
 
 export const getServerUrl = () => {
-    return "http://10.0.0.169:5000/";
+    return process.env.DOMAIN_NAME || "http://10.0.0.169:5000/";
 }
