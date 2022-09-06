53bc77a2a6ab39d0990635134124e7bd1ebe7bad
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2022-09-05 20:15:38 -0400

    changed to plan type live mode

--- src/utils/types/stripe.types.ts
+++ src/utils/types/stripe.types.ts
@@ -1,4 +1,4 @@
 export enum PlanType {
-    BASIC = "price_1LefXaATNTHRR4Uv8dC7KEdW", //"price_1LeKLVATNTHRR4UvsdkwPPV1",
-    PRO = "price_1LefXsATNTHRR4Uve3QdJI1g"//"price_1LeKQMATNTHRR4Uv232dfOic"
+    BASIC = "price_1LeKLVATNTHRR4UvsdkwPPV1",
+    PRO = "price_1LeKQMATNTHRR4Uv232dfOic"
 }
