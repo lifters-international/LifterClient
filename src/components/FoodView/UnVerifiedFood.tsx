24cef296f37c0013acda3e9358f31d808131c7b5
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2022-10-11 16:22:41 -0400

    allowed users to create their own food

---
+++ src/components/FoodView/UnVerifiedFood.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnUnVerifiedFood: React.FC = () => {
+    return (
+        <div className="UnVerifiedFood">
+            UnVerified
+        </div>
+    )
+}
+
+export default UnUnVerifiedFood;
